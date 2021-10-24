d836213b1a89b95b6e112a21f6c35e506382b9b2
cyruzzo <80426829+cyruzzo@users.noreply.github.com>
2021-10-24 12:37:55 +0200

    initial commit

---
+++ onconnect/app.js
@@ -0,0 +1,28 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+const AWS = require('aws-sdk');
+
+const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
+
+exports.handler = async event => {
+
+  const campaignId=event.queryStringParameters?event.queryStringParameters.campaign:"";
+  const objectId="conn#"+event.requestContext.connectionId;
+  const putParams = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      campaignId: campaignId,
+      objectId: objectId,
+      timestamp: Date.now(),
+    }
+  };
+
+  try {
+    await ddb.put(putParams).promise();
+  } catch (err) {
+    return { statusCode: 500, body: 'Failed to connect: ' + JSON.stringify(err) };
+  }
+
+  return { statusCode: 200, body: 'Connected.' };
+};
---
+++ sendmessage/app.js
@@ -0,0 +1,77 @@
+const AWS = require('aws-sdk');
+
+const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
+
+const { TABLE_NAME } = process.env;
+
+exports.handler = async event => {
+
+  const recvMessage=JSON.parse(event.body);
+  if(recvMessage.eventType=="custom/myVTT/keepalive")
+    return { statusCode: 200, body: 'Data sent.' };
+
+  const campaignId=recvMessage.campaignId;
+  const senderId=event.requestContext.connectionId;
+
+  try {
+    connectionData = await ddb.query({
+      TableName: TABLE_NAME,
+      KeyConditionExpression: "campaignId = :hkey and begins_with(objectId,:skey)",
+      ExpressionAttributeValues: {
+        ':hkey': campaignId,
+        ':skey': "conn#"
+      },
+    }).promise();
+  } catch (e) {
+    return { statusCode: 500, body: e.stack };
+  }
+  
+  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
+  });
+  
+  const postCalls = connectionData.Items.map(async ({ objectId }) => {
+    const connectionId= objectId.substring(5); // CONN#ID  to ID
+    if(connectionId==senderId){
+      return;
+    }
+    try {
+      await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: event.body }).promise();
+    } catch (e) {
+      if (e.statusCode === 410) {
+        console.log(`Found stale connection, deleting ${connectionId}`);
+        await ddb.delete({ TableName: TABLE_NAME, Key: { campaignId: campaignId, objectId: objectId } }).promise();
+      } else {
+        throw e;
+      }
+    }
+  });
+
+  // STORE TOKEN DATA IN DYNAMODB.
+  if(recvMessage.eventType=="custom/myVTT/token"){
+    const objectId="scenes#"+recvMessage.sceneId+"#tokens#"+recvMessage.data.id;
+    const putParams = {
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        campaignId: campaignId,
+        objectId: objectId,
+        data: recvMessage.data,
+        timestamp: Date.now(),
+      }
+    };
+    try {
+      await ddb.put(putParams).promise();
+    } catch (err) {
+      return { statusCode: 500, body: 'Failed to connect: ' + JSON.stringify(err) };
+    }
+  }
+
+  try {
+    await Promise.all(postCalls);
+  } catch (e) {
+    return { statusCode: 500, body: e.stack };
+  }
+
+  return { statusCode: 200, body: 'Data sent.' };
+};
